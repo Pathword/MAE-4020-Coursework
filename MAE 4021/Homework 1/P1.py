ca1722faa1e7599f36c047b327c3e790cacf2116
Pathword <dylancopley98@gmail.com>
2019-09-03 11:54:17 -0600

    Did 4021 section

---
+++ MAE 4021/Homework 1/P1.py
@@ -0,0 +1,15 @@
+"""
+Dylan Copley
+MAE 4021
+Homework 1
+Problem 1
+
+
+"""
+
+
+#defining function in2m as a function of inch
+def in2m(inch):
+    # nothing to declare, simply return value
+    return (inch*0.0254)
+
